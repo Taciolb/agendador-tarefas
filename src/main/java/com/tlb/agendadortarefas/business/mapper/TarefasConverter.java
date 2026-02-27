f8e729df67806123f0c34fe562556563affca731
Taciano <taciolb@gmail.com>
2026-02-26 22:05:22 -0300

    adicionados metodos Get por email e por periodo

--- src/main/java/com/tlb/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/tlb/agendadortarefas/business/mapper/TarefasConverter.java
@@ -4,10 +4,18 @@ import com.tlb.agendadortarefas.business.dto.TarefasDTO;
 import com.tlb.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
+
 
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
+
 }
